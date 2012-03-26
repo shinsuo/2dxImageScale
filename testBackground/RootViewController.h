0d15c0e2ffe5b08c47a6480394a29a201d4236e8
dingwenjie <administrator@dingwenjie.local>
2012-03-26 09:39:39 +0800

    Initial Commit

---
+++ testBackground/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  testBackground
+//
+//  Created by  on 12-3-26.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
